35b25dd46012b320f6c3287a2e4c2ec6ec6bf254
goodboi1234 <142526213+goodboi1234@users.noreply.github.com>
2023-11-27 18:17:34 +0530

    Update inherit_cars.cpp

--- inherit_cars.cpp
+++ inherit_cars.cpp
@@ -1,67 +1,44 @@
 #include <iostream>
-#include <string.h>
 #include <map>
 using namespace std;
-class Tata{
-    public:
-    map<string , string> elements;
-    Tata(){
-    elements["engine"] = {"engine1" , "engine2"};
-    elements["jumpers"]= {"loose" , "tight"};
-    elements["headlights"] = {"dull" , "bright"};
-    elements["gearbox"]  = {"auto" , "gearbox"};
-    elements["clutch"] = {"no" , "yes"};
-    elements["fuels"] = {"deisel" , "petroll"};
-    };
-    void doing(){
-        cout<<"the company is manufacturing cars";
-    };
-};
-class Nano:public Tata{
-    public:
-    map<string , string> nano_elements;
-    Nano(){
-    nano_elements["engine"] = elements["engine"][0];
-    nano_elements["jumpers"] = elements["jumpers"][0];
-    nano_elements["headlights"] = elements["headlights"][0];
-    nano_elements["gearbox"] = elements["gearbox"][1];
-    nano_elements["fuels"] = elements["fuels"][1];
-    nano_elements["size"] = "too small";   
-    };
-};
-class Nexon:public Tata{
-   public:
-    map<string , string> nex_elements;
-    Nexon(){
-   nex_elements["engine"] = elements["engine"][0];
-    nex_elements["jumpers"] = elements["jumpers"][1];
-    nex_elements["headlights"] = elements["headlights"][0];
-    nex_elements["gearbox"] = elements["gearbox"][1];
-    nex_elements["fuels"] = elements["fuels"][1];
-    nex_elements["size"] = "too small";   
-        
 
-    };
+class Tata {
+public:
+    map<string, string> elements;
+    Tata() {
+        elements["engine"] = "default_engine";
+        elements["jumpers"] = "default_jumpers";
+        elements["headlights"] = "default_headlights";
+        elements["gearbox"] = "default_gearbox";
+        elements["fuels"] = "default_fuels";
+        elements["size"] = "default_size";
+    }
 };
-class Jaguar:public Tata{
-    public:
-    map<string , string> jaguar_elements;
-    Jaguar(){
-    jaguar_elements["engine"] = elements["engine"][1];
-    jaguar_elements["jumpers"] = elements["jumpers"][1];
-    jaguar_elements["headlights"] = elements["headlights"][1];
-    jaguar_elements["gearbox"] = elements["gearbox"][0];
-    jaguar_elements["fuels"] = elements["fuels"][0];
-    jaguar_elements["size"] = "too small";   
-    };
-    void what_init(){
-        cout<<"manufactured by jrr group"<<endl<<"owned by Tata motors";
-    };
+
+class Jaguar : public Tata {
+public:
+    map<string, string> jaguar_elements;
+    Jaguar() {
+        jaguar_elements["engine"] = elements["engine"];
+        jaguar_elements["jumpers"] = elements["jumpers"];
+        jaguar_elements["headlights"] = elements["headlights"];
+        jaguar_elements["gearbox"] = elements["gearbox"];
+        jaguar_elements["fuels"] = elements["fuels"];
+        jaguar_elements["size"] = "too small";
+    }
+
+    void what_init() {
+        cout << "Manufactured by JRR Group" << endl << "Owned by Tata Motors";
+    }
 };
-int main(){
-    class Jaguar jaguar;
-    for(const auto& plain:jaguar.jaguar_elements){
-        cout<<plain.first<<":"<<plain.second<<endl;
-            };
+
+int main() {
+    Jaguar jaguar;
+    for (const auto& plain : jaguar.jaguar_elements) {
+        cout << plain.first << ": " << plain.second << endl;
+    }
+
+    jaguar.what_init();
+
     return 0;
 }
