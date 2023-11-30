9b57db8af1791d19a7db2c8cb032709a1bec5b2e
goodboi1234 <142526213+goodboi1234@users.noreply.github.com>
2023-11-30 19:04:14 +0530

    Update forth_project.cpp

--- forth_project.cpp
+++ forth_project.cpp
@@ -5,9 +5,9 @@ class Distance{
      int km;
     int m;
     public :
-    void seting(int km  , int m1){
-        this-> km =km+=m/1000;
-        this ->m = m-1000;
+    void seting(int km  , int m){
+        this-> km =km+m/1000;
+        this ->m = m%1000;
         
         
     };
@@ -27,6 +27,8 @@ int main()
     class Distance distance1 , distance2 , distance3;
     distance1.seting(3 , 1020);
     distance2.seting(2 , 2050);
+    distance1.get();
+    distance2.get();
     distance3 = distance1+distance2;
     distance3.get();
     return 0;
